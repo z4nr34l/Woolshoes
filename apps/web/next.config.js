19a8667f82e7f961f61a8513eca2e22b23418220
Z4NR34L <software@zanreal.net>
2023-11-14 14:47:59 +0100

    Minimal Next.js config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,15 +1,3 @@
-const path = require('path');
-
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    webpackBuildWorker: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias['@'] = path.join(__dirname, '/');
-    }
-
-    return config;
-  },
 };
